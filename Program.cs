666a68f48781cd88a92b7bd0c9d864edf5224250
egrl <egrrge@gmail.com>
2023-11-18 18:36:49 -0600

    primer cambio

--- Program.cs
+++ Program.cs
@@ -1,30 +1,16 @@
-﻿using System;
+﻿// 
+using System;
 
-namespace Actividad
+class Program
 {
-    class Program
+     static void Main()
     {
-        static void Main()
-        {
-            for (int i = 1; i <= 100; i++)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
-            }
-        }
-    }
+         for (int i = 1; i <= 100; i++)
+         {
+             string output = "";
+             if (i % 3 == 0) output += "Fizz";
+             if (i % 5 == 0) output += "Buzz";
+             Console.WriteLine(output != "" ? output : i.ToString());
+         }
+     }
 }
